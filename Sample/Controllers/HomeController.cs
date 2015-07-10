76ff98efa7cf383acf387613c959b0d446bf9146
Alexander Kouznetsov <inbox@unconnected.info>
2015-07-10 23:42:05 +0300

    First commit
    I ran it and it worked.

---
+++ Sample/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Unconnected.Mvc.Outputcache;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// This action will get varyByParam attribute from web.config.
+        /// Code will not run until cache expiration.
+        /// </summary>
+        /// <returns></returns>
+        [ParameterizedOutputCache(CacheProfile = "test")]
+        public ActionResult Index()
+        {
+            var p = new ParameterizedOutputCacheAttribute();
+            return View();
+        }
+
+        /// <summary>
+        /// This action will ignore varyByParam attribute from web.config.
+        /// But you can specify it directly and code will not run until cache expiration.
+        /// </summary>
+        /// <returns></returns>
+        [OutputCache(CacheProfile = "test", VaryByParam="none")]
+        public ActionResult CachedAction()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// This action will ignore varyByParam attribute from web.config.
+        /// Code will run any time you add random query string to request.
+        /// </summary>
+        /// <returns></returns>
+        [OutputCache(CacheProfile="test")]
+        public ActionResult NoProtectionFromRandom()
+        {
+            return View();
+        }
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,23 @@
+﻿First of all: LICENSE.
+Actually, there’s no license at all. 
+Use this code any way you like: private, commercial, for fun or profit, for good or evil. 
+But! At your own risk. No warranty or safety guaranteed. 
+You can modify or redistribute it freely and widely without any limitations. 
+I will be thankful to you if you keep my credentials at the top of the class, but I do not insist. 
+If this code saved you time and nerves – just treat me a beer if we ever met.
+
+Package contents.
+readme.txt – text you are reading right now.
+ParameterizedOutputCacheAttribute.cs – code for ParameterizedOutputCacheAttribute
+Sample\Controllers\HomeController.cs – usage sample
+Sample\Web.config – output cache profile definition sample.
+
+Purpose.
+OutputCache attribute is very useful to manage output cache. 
+It has a bug (or maybe feature) that you have to define varyByParam collection with attribute. 
+Those defined at web.config are ignored. So this code fixes the issue.
+
+Alexander Kouznetsov
+www.unconnected.info / inbox@unconnected.info
+Saint-Petersburg. Russia.
+2015-07-10
